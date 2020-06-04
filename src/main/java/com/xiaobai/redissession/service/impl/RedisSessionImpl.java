490af2352131a23efbd073be22ac5896243d0452
yin_zhj <yin_zhj@murongtech.com>
2020-06-04 16:04:23 +0800

    添加日志

--- src/main/java/com/xiaobai/redissession/service/impl/RedisSessionImpl.java
+++ src/main/java/com/xiaobai/redissession/service/impl/RedisSessionImpl.java
@@ -2,6 +2,8 @@ package com.xiaobai.redissession.service.impl;
 
 import com.xiaobai.redissession.exception.ExpireTypeException;
 import com.xiaobai.redissession.service.RedisSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -25,6 +27,8 @@ public class RedisSessionImpl implements RedisSession {
 
     private TimeUnit expireType;
     private Long expire;
+
+    private Logger logger = LoggerFactory.getLogger("com.xiaobai.redissession.service.impl.RedisSessionImpl");
 
     public RedisSessionImpl(String expireType, Long expire) {
         this.expire = expire;
@@ -51,16 +55,20 @@ public class RedisSessionImpl implements RedisSession {
 
     @Override
     public void set(HttpServletResponse response, Map<String, String> map) {
+        logger.info("set session start......");
         SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss");
         String jSessionId = sf.format(new Date());
+        logger.info("session id:{}", jSessionId);
         redisTemplate.opsForHash().putAll(jSessionId, map);
         redisTemplate.expire(jSessionId, expire, expireType);
         Cookie cookie = new Cookie(ID, jSessionId);
         response.addCookie(cookie);
+        logger.info("set session end");
     }
 
     @Override
     public String get(HttpServletRequest request, String key) {
+        logger.info("get session start......");
         Cookie[] cookies = request.getCookies();
         String jSessionId = null;
         if(null != cookies) {
@@ -71,18 +79,27 @@ public class RedisSessionImpl implements RedisSession {
             }
         }
         if(null != jSessionId) {
+            logger.info("session id:{}", jSessionId);
             if(!redisTemplate.opsForHash().hasKey(jSessionId, key)) {
+                logger.info("key:{},value:{}", key, null);
+                logger.info("get session end");
                 return null;
             } else {
-                return (String)redisTemplate.opsForHash().get(jSessionId, key);
+                String value = (String)redisTemplate.opsForHash().get(jSessionId, key);
+                logger.info("key:{},value:{}", key, value);
+                logger.info("get session end");
+                return value;
             }
         } else {
+            logger.info("session is null");
+            logger.info("get session end");
             return null;
         }
     }
 
     @Override
     public void removeSession(HttpServletRequest request, HttpServletResponse response) {
+        logger.info("remove session...");
         Cookie[] cookies = request.getCookies();
         String jSessionId = null;
         if(null != cookies) {
@@ -93,6 +110,7 @@ public class RedisSessionImpl implements RedisSession {
                 }
             }
             if(null != jSessionId) {
+                logger.info("session id:{}", jSessionId);
                 redisTemplate.delete(jSessionId);
                 Cookie cookie = new Cookie(ID, null);
                 cookie.setMaxAge(0);
@@ -100,6 +118,7 @@ public class RedisSessionImpl implements RedisSession {
                 response.addCookie(cookie);
             }
         }
+        logger.info("remove session end");
     }
 
     @Override
